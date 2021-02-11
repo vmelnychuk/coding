ca31d94516b8f9dd886d19bd2090ef26cc4e50bd
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2021-02-11 15:47:05 +0200

    spring-core: add property

--- spring-core/src/main/java/io/learn/springcore/configuration/AppConfig.java
+++ spring-core/src/main/java/io/learn/springcore/configuration/AppConfig.java
@@ -2,9 +2,11 @@ package io.learn.springcore.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"io.learn.springcore"})
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
 }
--- spring-core/src/main/java/io/learn/springcore/service/SimpleServiceMainImpl.java
+++ spring-core/src/main/java/io/learn/springcore/service/SimpleServiceMainImpl.java
@@ -3,12 +3,16 @@ package io.learn.springcore.service;
 import io.learn.springcore.repository.SimpleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
 public class SimpleServiceMainImpl implements SimpleService {
+
+    @Value("${name}")
+    private String name;
 
     private SimpleRepository simpleRepository;
 
@@ -19,7 +23,7 @@ public class SimpleServiceMainImpl implements SimpleService {
 
     @Override
     public void doTask() {
-        System.out.println("Call doTask for " + this.getClass().getSimpleName());
+        System.out.println("Call doTask for " + this.getClass().getSimpleName() + " by user [" + name + "]");
         simpleRepository.doQuery();
     }
 }
